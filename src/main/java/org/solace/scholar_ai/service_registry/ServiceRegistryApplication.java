1b0c7a444a7e38a431c9f0b7de31b8dc9c5da4fc
Legend-2727 <2005042@ugrad.cse.buet.ac.bd>
2025-10-23 12:36:43 +0600

    Add javadoc to main application class
    Added some documentation to explain what this service does

--- src/main/java/org/solace/scholar_ai/service_registry/ServiceRegistryApplication.java
+++ src/main/java/org/solace/scholar_ai/service_registry/ServiceRegistryApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Main entry point for the ScholarAI Service Registry.
+ * 
+ * This is our central service discovery server using Netflix Eureka.
+ * All microservices register here so they can find each other.
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ServiceRegistryApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
 }
