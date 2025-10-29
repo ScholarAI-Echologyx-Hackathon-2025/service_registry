32959731272ce8e3b0d09ac14055c74d83562dba
Legend-2727 <2005042@ugrad.cse.buet.ac.bd>
2025-10-30 00:04:18 +0600

    Add startup banner with service info

--- src/main/java/org/solace/scholar_ai/service_registry/ServiceRegistryApplication.java
+++ src/main/java/org/solace/scholar_ai/service_registry/ServiceRegistryApplication.java
@@ -1,8 +1,12 @@
 package org.solace.scholar_ai.service_registry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * Main entry point for the ScholarAI Service Registry.
@@ -14,8 +18,21 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class ServiceRegistryApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(ServiceRegistryApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegistryApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ServiceRegistryApplication.class, args);
+		Environment env = context.getEnvironment();
+		
+		String port = env.getProperty("server.port", "8761");
+		String profile = env.getProperty("spring.profiles.active", "default");
+		
+		log.info("\n----------------------------------------------------------\n\t" +
+				"Service Registry is running!\n\t" +
+				"Profile(s): \t{}\n\t" +
+				"Dashboard: \thttp://localhost:{}\n" +
+				"----------------------------------------------------------",
+				profile, port);
 	}
 
 }
